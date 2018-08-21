eed52442554f8adca7476ce97a2abfba49b07563
JamesSab1 <jimsab69@hotmail.com>
2018-08-21 20:51:46 +0100

    basic tutorial finished

--- test.c
+++ test.c
@@ -1,13 +1,13 @@
-# define something
-/* some 
+#define something
+ /* some 
 
-	quotes */
-//some more quotes
+	quotes */
+    //some more quotes
 //a few more
 	9 + 9 = 18
 char variable[] = "Something";
 int var9 = 99;
-s = 'somet"hi"ng';
+ s = 'somet"hi"ng';
 
 
 void function(char *s)
@@ -16,13 +16,13 @@ void function(char *s)
 }
 
 
-lines and lines 
-lines and lines and lines lines and lines and lineslines and lines and lineslines and lines and linesv
+/* lines and lines 
+ lines and lines and lines lines and lines and lineslines and lines and lineslines and lines and linesv
 lines and lines and lines
 lines and lines and lines
 lines and lines and lines
 
-lines and lines and lines
+lines and*/ lines and lines
 lines and lines and lines
 v
 v
